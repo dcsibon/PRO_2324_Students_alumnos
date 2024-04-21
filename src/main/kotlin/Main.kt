64da4d75aef727209aa1b01d4cd8d7e44ec78ace
dcsibon <dcsibon@gmail.com>
2024-04-21 19:37:00 +0200

    Versión terminada

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,83 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.WindowState
+
+
+@Composable
+@Preview
+fun App() {
+
+    var user by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val buttonEnabled = user.isNotBlank() && password.isNotBlank()
+
+    MaterialTheme {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(10.dp, alignment = Alignment.CenterVertically),
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+            OutlinedTextField(
+                value = user,
+                onValueChange = { user = it },
+                label = { Text("Usuario") }
+            )
+
+            var passVisible by remember { mutableStateOf(false) }
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Contraseña") },
+                visualTransformation = if (passVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+                    IconToggleButton(
+                        checked = passVisible,
+                        onCheckedChange = { passVisible = it }
+                    ) {
+                        Icon(
+                            imageVector = if (passVisible) Icons.Default.VisibilityOff else Icons.Default.Visibility,
+                            contentDescription = null
+                        )
+                    }
+                }
+            )
+
+            Button(
+                onClick = {
+                    user = ""
+                    password = ""
+                },
+                enabled = buttonEnabled
+            ) {
+                Text(text = "Login")
+            }
+        }
+    }
+}
+
+fun main() = application {
+    val windowState = WindowState(size = DpSize(1200.dp, 800.dp))
+
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Mi Login",
+        state = windowState
+    ) {
+        App()
+    }
+}

d115712c304b7ee2bb7141070de630fcc10ec98f
Diego Cano Sibón <dcsibon@gmail.com>
2024-04-22 16:30:21 +0200

    He añadido un comentario...

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,6 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
